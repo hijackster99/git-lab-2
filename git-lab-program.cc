9aa9e726bdf0adf59541b775b6210ec0ea4677cd
hijackster99 <jg380218@ohio.edu>
2019-09-04 16:48:56 -0400

    Initial Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,21 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jack Gagnon
+ *        Date: 9/4/19
+ * Description: "Hello Git!"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+
+    cout << "Hello Git!" << endl;
+
+    return 0;
+}// main
